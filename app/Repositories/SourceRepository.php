54300cf2a29ece316bf3897d777898c553ed56c4
Krystian Duma <git@kduma.tk>
2015-03-01 18:42:26 +0100

    Course Entity

--- app/Repositories/SourceRepository.php
+++ app/Repositories/SourceRepository.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Repositories;
+use App\Course;
 use GuzzleHttp\Client;
 use GuzzleHttp\Subscriber\Oauth\Oauth1;
 
@@ -20,14 +21,42 @@ class SourceRepository {
 
     public function get()
     {
+        $only_existing_courses = [];
         foreach ($this->getRepositoriesList() as $repo) {
             if(substr($repo['name'], 0, 7) != 'Kurs - ')
                 continue;
+
             $branches = $this->getBranches($repo['full_name']);
             foreach ($branches as $branch_name => $branch) {
+                $slug = explode('/', $repo['full_name'], 2);
+                $slug = $slug[1];
+//                $slug = $repo['full_name'];
+
+                try{
+                    $Course = Course::whereSlug($slug)->whereVersion($branch_name)->withTrashed()->firstOrFail();
+                    $Course->restore();
+                }catch (\Exception $e){
+                    $Course = new Course();
+                    $Course->slug = $slug;
+                    $Course->version = $branch_name;
+                }
+                $only_existing_courses[] = [$slug, $branch_name];
+                $Course->name = $repo['name'];
+                $Course->save();
+
+
+
                 $this->download($repo['full_name'], $branch_name);
             }
         }
+
+
+        //Kasowanie pozostałych kursów
+        $x = Course::withTrashed();
+        foreach ($only_existing_courses as $c) {
+            $x->WhereRaw('NOT (slug = ? and version = ?)', $c);
+        }
+        $x->delete();
     }
 
     public function getRepositoriesList()
@@ -66,9 +95,11 @@ class SourceRepository {
     /**
      * @param $repoFullName
      * @param $branch
+     * @return array
      */
     public function download($repoFullName, $branch)
     {
+        $prased = [];
         $repoName = explode('/', $repoFullName);
         $repoName = $repoName[1];
 
@@ -80,18 +111,24 @@ class SourceRepository {
             while ($zip_entry = zip_read($zip)) {
                 $file_name = zip_entry_name($zip_entry);
                 $file_name = explode('/', $file_name, 2);
-                $file_name = $repoName.'/'.$branch.'/'.$file_name[1];
+//                $file_name = $repoName.'/'.$branch.'/'.$file_name[1];
+                $file_name = $file_name[1];
 
-                if (substr($file_name, '-1') == '/') {
-                    \Storage::disk('local')->makeDirectory($file_name);
-                } else {
+                if (substr($file_name, '-1') != '/' && $file_name != '') {
+
                     if (zip_entry_open($zip, $zip_entry)) {
-                        $contents = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
-                        \Storage::disk('local')->put($file_name, $contents);
-                        zip_entry_close($zip_entry);
+                        if(substr($file_name, '-3') == '.md') {
+                            $prased['course'][substr($file_name, '0', '-3')] = zip_entry_filesize($zip_entry);
+                            $contents = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+                            zip_entry_close($zip_entry);
+                        }else{
+                            $prased['other'][$file_name] = zip_entry_filesize($zip_entry);
+                        }
                     }
                 }
             }
         }
+
+        return($prased);
     }
 }
